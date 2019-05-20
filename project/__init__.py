66f2aaaec28d1b15c2e23c0c79eaea6fee974281
Luo Xiangyong <luoxiangyong@topgridcloud.com>
2019-05-20 16:56:15 +0800

    添加数据库支持

--- project/__init__.py
+++ project/__init__.py
@@ -1,7 +1,8 @@
 #
 import os,sys
+import datetime
 from flask import Flask,jsonify
-
+from flask_sqlalchemy import SQLAlchemy
 # 初始化app
 app = Flask(__name__)
 
@@ -10,11 +11,28 @@ app = Flask(__name__)
 app_settings = os.getenv("APP_SETTINGS")
 app.config.from_object(app_settings)
 
+# 初始化数据库
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=False, nullable=False)
+    create_at = db.Column(db.DateTime, nullable=False)
+    
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+        self.create_at = datetime.datetime.utcnow()
+        
+
 @app.route('/ping', methods = ['GET'])
 def ping_pong():
-	return jsonify({
-		'status':'success',
-		'message':'pong!'
-	})
+    return jsonify({
+        'status':'success',
+        'message':'pong!'
+    })
 
 print(app.config,file=sys.stderr)
